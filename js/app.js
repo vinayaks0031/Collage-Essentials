f79466dd20b2f4eb8dcf5fea60c5de806a3b8451
vinayak <vinayaks0031@gmail.com>
2021-05-04 17:37:04 +0530

    fixed error of email

--- js/app.js
+++ js/app.js
@@ -77,20 +77,19 @@ function leftslide() {
   }
 }
 
+
 let Params = {
   to_name: 'Vinayak Sharma',
   from_name: '',
-  message_html: ''
+  message: ''
 };
 
-let message;
 let btnmsg = document.getElementById("btnmsg");
 btnmsg.addEventListener('click', getmsg);
 
 function getmsg(e) {
   e.preventDefault();
-  Params.message_html = document.getElementById("formquery").value;
-  // console.log(formquery);
+  Params.message = document.getElementById("formquery").value;
   let form1 = document.getElementById("contect-us");
   form1.style.display = "none";
   let form2 = document.getElementById("contect-us-email");
@@ -103,12 +102,11 @@ formSubmit.addEventListener("submit", getemail);
 function getemail(e) {
   e.preventDefault();
   Params.from_name = document.getElementById("gmailquery").value;
-  // console.log(message, gmail);
   let p = document.createElement("p");
   p.innerText = "Thankyou we'll contact you shortly!"
   p.style.fontSize = "30px";
   formSubmit.replaceWith(p);
-  emailjs.send('service_20auxhw', 'template_gi9syv2', Params)
+  emailjs.send('service_20auxhw', 'template_09mo43a', Params)
     .then(function (response) {
       console.log('SUCCESS!', response.status, response.text);
     }, function (error) {
