23bff9e668dbddf25aee1a4e020a9760e721f863
vinayak <vinayaks0031@gmail.com>
2021-05-23 15:34:58 +0530

    one more page added

--- js/book.js
+++ js/book.js
@@ -406,3 +406,18 @@ anime.timeline({ loop: false })
         easing: "easeOutExpo",
         delay: 1000
     });
+// anime.timeline({ loop: false })
+//     .add({
+//         targets: '.ml15 .word',
+//         scale: [14, 1],
+//         opacity: [0, 1],
+//         easing: "easeOutCirc",
+//         duration: 500,
+//         delay: (el, i) => 800 * i
+//     }).add({
+//         targets: '.ml15',
+//         opacity: 1,
+//         duration: 1000,
+//         easing: "easeOutExpo",
+//         delay: 1000
+//     });
